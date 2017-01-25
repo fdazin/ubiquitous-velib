84eabb19e6f936efbed616bf68d2dfbdc0f66cca
FREAL-SAISON <s.frealsaison@gmail.com>
2017-01-25 21:50:20 +0100

    initial commit of the file containing general demonstration of functions

---
+++ velib_group-poc_file.R
@@ -0,0 +1,75 @@
+
+require(reshape2)
+library(leaflet)
+
+# exemples de visualisations testées
+
+
+JCDecaux.data_in <- read.csv2("stations-velib-disponibilites-en-temps-reel.csv")
+
+JCDecaux.data_prep <- data.frame(JCDecaux.data_in)
+JCDecaux.data_viz = transform(JCDecaux.data_prep, 
+                              position = colsplit(position, pattern=",", names = c('lat', 'lng')))
+JCDecaux.data_viz = cbind (JCDecaux.data_viz, JCDecaux.data_viz$position)
+
+head(JCDecaux.data_viz)
+
+#names(JCDecaux.data_viz)[names(JCDecaux.data_viz) == 'position.lng'] <- 'lng'
+#names(JCDecaux.data_viz)[names(JCDecaux.data_viz) == 'position.lat'] <- 'lat'
+
+
+# Exemple 1 avec leaflet
+# m <- leaflet(JCDecaux.data_viz) %>%
+#   addTiles() %>% 
+#   addCircles(lat = ~lat, lng = ~lng, radius = ~bike_stands) %>%
+#   addMarkers(lng = 2.2965736, lat = 48.8152334, popup="Cepe - prévoir les tablettes de chocolat pour ressortir")
+
+
+
+# autre exemple de visu de carto en alternative
+## Map from URL : http://maps.googleapis.com/maps/api/staticmap?center=houston&zoom=14&size=640x640&scale=2&maptype=terrain&language=en-EN&sensor=false
+## Information from URL : http://maps.googleapis.com/maps/api/geocode/json?address=houston&sensor=false
+
+
+# Exemple 2 avec rbokeh
+# library(rbokeh)
+# 
+# gmap(lat = 40.73306, lng = -73.97351,zoom =12,#lat = 48.8152334, lng = 2.2965736, zoom = 12,
+#      width = 680, height = 600 , map_type = "roadmap") %>%
+#   ly_points(lat, lng, data = JCDecaux.data_viz, hover = c(status, bike_stands))
+# 
+# gmap(lat = 40.73306, lng = -73.97351, zoom = 12, width = 680, height = 600)
+
+
+### Fichier 2
+OpenDataParis.data_in <- read.csv2("Paris.csv", sep = ",")
+head(OpenDataParis.data_in)
+# moins riche que JCDecaux
+
+### Fichier 3
+library(jsonlite)
+# json_file <- "http://api.worldbank.org/country?per_page=10&region=OED&lendingtype=LNX&format=json"
+json_file1 <- "input_Paris.json"
+json_data1 <- read_json(path=json_file1, simplifyVector = TRUE)
+head(json_data1)
+
+# #### Fichier 4 # json invalide - prendre le data_all_Paris avec date.gz à la place
+# json_file2 <- "data_all_Paris.json"
+# json_data2 <- read_json(path=json_file2, simplifyVector = TRUE, header = T)
+# head(json_data2)
+
+library(anytime)
+anytime(1352068320)
+
+# #### Fichier 5 - json en streaming
+# json_file3 <- "input_Paris.json"
+# json_data3 <- read_json(path=json_file3, simplifyVector = TRUE)
+# head(json_data3)
+
+json_file <- "data_all_Paris.jjson_2017-01-01-1483248351.gz"
+dat <- fromJSON(sprintf("[%s]", paste(readLines(json_file), collapse=",")))
+dim(dat)
+
+# On obtient une liste contenant l'ensemble des df des données collectées sur 1 mois, le pas temporel est de 20 minutes.
+
+
