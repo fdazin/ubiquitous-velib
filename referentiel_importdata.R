2408b00af1510aa4fd8c7e2a935e78d218028aee
FREAL-SAISON <s.frealsaison@gmail.com>
2017-02-02 20:57:35 +0100

    Commit initial du chargement du referentiel à partir de données opendata
    descriptif complet des stations disponible en ligne sous forme de fichier csv. Le code developpé met les informations à disposition dans une base sqlite et en memoire sous forme de dataframe.

---
+++ referentiel_importdata.R
@@ -0,0 +1,36 @@
+library(jsonlite)
+library(sqldf)
+library(plyr)
+library(DBI)
+library(dtplyr)
+library(data.table)
+library(magrittr)
+library(treemap)
+library(lubridate)
+
+
+# TODO : initialiser la base sqlite si elle est absente, et travailler en append si des données sont déja présentes.
+#         Prévoir de nettoyer la base pour ne garder que les lignes uniques.
+
+
+data_fromSQLITE_to_df <- function(SQLite_db, tbl_name) {
+  con <- dbConnect(RSQLite::SQLite(), dbname=SQLite_db )
+  df <- dbReadTable(con, tbl_name)
+  dbDisconnect(con)
+  return (df)
+}
+
+###############################################################################
+# Comment convertire le json intitial et générer les fichiers SQLITE ... lent
+setwd("~/Documents/dev/Cepe/ubiquitous-velib")
+# Placer les données dans un répertoire data/ à côté du script
+csv_file <- 'data/velib_a_paris_et_communes_limitrophes.csv'
+SQLite_db <- "SQLiteData/Test.sqlite"
+
+dat_full <- read.csv2(csv_file)
+con <- dbConnect(RSQLite::SQLite(), dbname=SQLite_db)
+dbWriteTable(con, name="ref_data", value = dat_full , row.names = FALSE)
+
+###############################################################################
+tbl_name <- "ref_data"
+referentiel <- data_fromSQLITE_to_df(SQLite_db, tbl_name)
